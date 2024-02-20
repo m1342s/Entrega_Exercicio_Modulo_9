798be28ee1016ba29b20ea5e488860100291d1b7
Matheus Viersa <matheusviersa995@hotmail.com>
2024-02-20 10:35:48 -0300

    Entrega corrigida

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-console.log("oi")
 $(document).ready(function () {
     $(".btn").click(function(){
         $("form").on("submit",function(e){
             e.preventDefault()
             const nomeTarefa=$("#tarefa").val()
-            console.log(nomeTarefa)
             const novaTarefa=$("<li></li>")
-        $(`<input value=${nomeTarefa}>`).appendTo(novaTarefa)
+        $(`<input class="desc" value=${nomeTarefa}>`).appendTo(novaTarefa)
         $(novaTarefa).appendTo("ul")
+        $(".desc").click(function() {
+            $(".desc").css("text-decoration","line-through")
+        })
         })
     })
 })
